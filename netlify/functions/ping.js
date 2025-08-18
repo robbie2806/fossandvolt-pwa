8d7a27276dbb88051fe2970f0aedc85d0a477021
robbie2806 <robertcalvert42@gmail.com>
2025-08-18 19:11:44 +1000

    Update ping.js

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,10 +1,3 @@
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: JSON.stringify({ ok: true, msg: "pong", time: new Date().toISOString() })
-  };
+  return { statusCode: 200, body: JSON.stringify({ ok: true, msg: "pong" }) };
 };
